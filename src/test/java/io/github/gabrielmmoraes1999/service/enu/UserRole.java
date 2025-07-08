c44892501505e402b236caac1653cbc34d894ee1
Gabriel Moraes <gabrielmmoraes1999@icloud.com>
2025-07-08 20:07:14 -0300

    remove test

--- src/test/java/io/github/gabrielmmoraes1999/service/enu/UserRole.java
+++
@@ -1,6 +0,0 @@
-package io.github.gabrielmmoraes1999.service.enu;
-
-public enum UserRole {
-    ADMIN,
-    USER;
-}
--- src/test/java/io/github/gabrielmmoraes1999/service/repository/ClientRepository.java
+++
@@ -1,8 +0,0 @@
-package io.github.gabrielmmoraes1999.service.repository;
-
-import io.github.gabrielmmoraes1999.db.DBRepository;
-import io.github.gabrielmmoraes1999.service.entity.Client;
-
-public interface ClientRepository extends DBRepository<Client, Integer> {
-
-}
