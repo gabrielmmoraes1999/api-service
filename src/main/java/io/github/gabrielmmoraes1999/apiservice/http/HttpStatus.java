b5d27f65acb64266db22149e956382a7c3632826
Gabriel Moraes <gabrielmmoraes1999@icloud.com>
2025-07-11 22:33:28 -0300

    Update HttpStatus.java

--- src/main/java/io/github/gabrielmmoraes1999/apiservice/http/HttpStatus.java
+++ src/main/java/io/github/gabrielmmoraes1999/apiservice/http/HttpStatus.java
@@ -11,6 +11,7 @@ public enum HttpStatus {
     FORBIDDEN(403, "Forbidden"),
     NOT_FOUND(404, "Not Found"),
     METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
+    NOT_ACCEPTABLE(406, "Not Acceptable"),
     CONFLICT(409, "Conflict"),
 
     INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
