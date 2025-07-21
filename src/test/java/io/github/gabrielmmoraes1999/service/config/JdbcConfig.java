bfe3f5f00b5b8049aa13a2cacc5fdd1d8bf6975a
Gabriel Moraes <gabrielmmoraes1999@icloud.com>
2025-07-20 21:48:32 -0300

    Update JdbcConfig

--- src/test/java/io/github/gabrielmmoraes1999/service/config/JdbcConfig.java
+++ src/test/java/io/github/gabrielmmoraes1999/service/config/JdbcConfig.java
@@ -1,8 +1,11 @@
 package io.github.gabrielmmoraes1999.service.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import io.github.gabrielmmoraes1999.apiservice.annotation.Bean;
 import io.github.gabrielmmoraes1999.apiservice.annotation.Component;
 import io.github.gabrielmmoraes1999.apiservice.security.oauth2.RegisteredClientJDBC;
+import io.github.gabrielmmoraes1999.db.ConnectionPoolManager;
 import io.github.gabrielmmoraes1999.db.DataBase;
 
 @Component
@@ -10,13 +13,22 @@ public class JdbcConfig {
 
     @Bean
     public void createRegisteredClientJdbc() {
-        try {
-            Class.forName("org.sqlite.JDBC").newInstance();
-            DataBase.createConnection("jdbc:sqlite:database.db");
-            RegisteredClientJDBC.setConnection(DataBase.getConnection());
-        } catch (Exception ignore) {
+        HikariDataSource hikariDataSource = getHikariDataSource();
+        ConnectionPoolManager.setHikariDataSource(hikariDataSource);
+        RegisteredClientJDBC.setHikariDataSource(hikariDataSource);
+    }
 
-        }
+    private HikariDataSource getHikariDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName("org.sqlite.JDBC");
+        config.setJdbcUrl("jdbc:sqlite:database.db");
+
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(2);
+        config.setConnectionTimeout(30000); // 30s
+        config.setIdleTimeout(600000);      // 10min
+        config.setMaxLifetime(1800000);     // 30min
+        return new HikariDataSource(config);
     }
 
 }
